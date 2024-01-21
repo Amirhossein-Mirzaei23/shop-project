a1e37669e236a3fe4f405e47665b3e1d2873cbbd
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-01-21 21:05:23 +0330

    set imge zoom to the slider image

--- components/indexslideshow.vue
+++ components/indexslideshow.vue
@@ -1,54 +1,70 @@
 <template>
     <div class="w-full h-full grid grid-cols-3" :style="`background-image:url()`">
       <div id="sliderImageElem" class="fadeIn flex items-center overflow-hidden">
-    <img id="carsoulImg" @mousemove="dynamicScale" :src="slide.images[0]" class="max-w-80 w-80 max-h-80 m-auto" />
-      </div>
-      
-      <div class="col-span-2 h-5/6 w-11/12 m-auto">
-      <h1 class="text-right">{{slide.title}}</h1>
-      <br>
-      <p>{{slide.description}}</p>
-      <p>{{slide.price}}</p>
+      <div id="carsoulImg" class="p-2 bg-slate-400 m-auto rounded-lg">
+       <img  @mousemove="xClient" @mouseleave="DefaultImageMargin" :style="{ marginLeft:`${x}px`,marginTop:`${y}px`}" :src="slide.images[0]" class="max-w-80 w-80 max-h-80 m-auto" />
       </div>
     </div>
+      
+      <div class="col-span-2 h-5/6 w-11/12 m-auto grid grid-rows-4">
+
+      <h1 class="text-right font-serif text-5xl">{{slide.title}}</h1>
+     
+      <p class="text-slate-600 font-thin text-left row-span-2 mt-5 ">{{slide.description}}</p>
+      
+      <p class="w-1/4 h-min font-sans text-center text-5xl text-red-600 shadow-sm shadow-teal-600">{{slide.price}}<span class="text-3xl">$</span></p>
+
+      </div>
+       
+    </div>
+ 
 </template>
+
 
 <script setup>
 import { ref, watch } from 'vue'
 
 
 
+
 const {slide}=defineProps(['slide'])
 console.log(slide);
 
-let x=0
-let y=ref(0)
+
 let color='red'
- let carsoulImg= document.getElementById("carsoulImg")
+let carsoulImg= document.getElementById("carsoulImg")
 
-function dynamicScale(event){
-   x=event.clientX;
-   y=event.clientY; 
+let x=ref(0)
+let y=ref(0)
 
-console.log(x);
+  function xClient(event){
+    let clientX=event.clientX
+    let clientY=event.clientY
+    console.log(screen.availWidth);
+  if ((Math.abs(clientX)/340)>.5) {
+    clientX=-clientX
+  }
+   if ((Math.abs(clientY)/340)>.5) {
+    clientY=-clientY
+   
+  }
+  x.value=clientX/3;
+  y.value=clientY/3;
+ }
+ function DefaultImageMargin(){
+  x.value=0
+  y.value=0
+ }
+//  setInterval(() => {
+//    console.log(  x.value,y.value);
+//  }, 2000);
 
-return{x,y}
-
-}
-
-
-watch(x,(newX)=>{
-  console.log(newX);
-})
-
-setTimeout(() => {
-  console.log(x);
-}, 5000);
 </script>
 
 <style scoped>
-#carsoulImg{
-  background-color: v-bind(color);
+#carsoulImg:hover{
+  scale:1.5;
+
 }
 
 .backgroundImg{
---
+++ composables/useImageZoom.js
@@ -0,0 +1,8 @@
+export default (x,y,)=>{
+    let xclient
+  //  let yclient
+    xclient=x
+  //  yclient=y
+   
+    return(xclient)
+}
