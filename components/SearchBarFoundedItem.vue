1b99de298739a9a7acc50183bcb1eedf6efc039d
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-02-04 16:39:38 +0330

    create search bar fetch api

---
+++ components/SearchBarFoundedItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-rose-200 py-1" v-for="item in index " :key="item">
+        <li class=" bg-slate-200 mt-1">{{item.title}}</li>
+    </div>
+</template>
+
+<script setup>
+
+const {index}=defineProps(['index'])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
